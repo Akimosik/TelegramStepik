4df85767705768e2f89cde91f43d1c7232df884a
Dmitry Akimushkin <akim.bryansk@gmail.com>
2023-04-03 19:50:11 +0300

    for home

--- some_file.py
+++ some_file.py
@@ -1,12 +1,42 @@
 import requests
-import pprint
+import time
 
-api_url = ' http://numbersapi.com/43'
 
-response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '6234301239:AAFSeaEe1ryTtnD8vhS75GlJHbjiCXtVyh4'
+TEXT: str = 'Ура! Классный апдейт!'
+MAX_COUNTER: int = 100
 
-if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-    print(response.text)
-else:
-    print(response.status_code)    # При другом коде ответа выводим этот код
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+API_CATS_URL: str = 'https://random.dog/woof.json'
+BOT_TOKEN: str = '6234301239:AAFSeaEe1ryTtnD8vhS75GlJHbjiCXtVyh4'
+ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
+
+offset: int = -2
+counter: int = 0
+cat_response: requests.Response
+cat_link: str
+
+
+while counter < 100:
+    print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+    time.sleep(1)
+    counter += 1
 
